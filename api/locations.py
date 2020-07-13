4e7569f3582de7e047c785111eef29343719af16
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-07-13 22:24:52 +0900

    Update locations.py

--- api/locations.py
+++ api/locations.py
@@ -6,10 +6,10 @@ CH3 = '서울';  LOC3 = 'Seoul'
 CH4 = '여수';  LOC4 = 'Yeosu'
 CH5 = '전주';  LOC5 = 'Jeonju'
 CH6 = '제주';  LOC6 = 'Jeju'
-CH7 = '울산';  LOC7 = 'Ulsan'
+CH7 = '울릉도';  LOC7 = 'Ulleungdo'
 CH8 = '남해';  LOC8 = 'Namhae'
 CH9 = '통영';  LOC9 = 'Tongyeong'
-CH10 = '경주';  LOC10 = 'Gyeongju'
+CH10 = '냠원';  LOC10 = 'Namwon'
 
 LOCATION_CHOICE = [
     (CH1, LOC1),
@@ -22,4 +22,3 @@ LOCATION_CHOICE = [
     (CH8, LOC8),
     (CH9, LOC9),
     (CH10, LOC10)
-]
