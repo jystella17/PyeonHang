5516b76758b5a70511608bf38146b3a35b1c1f3f
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-07-16 21:41:27 +0900

    Update urls.py

--- api_server/urls.py
+++ api_server/urls.py
@@ -11,6 +11,7 @@ from drf_yasg import openapi
 schema_url_patterns = [
     path('api/', include('api.urls'))
 ]
+'''
 schema_view = get_schema_view(
     openapi.Info(
         title='Snippets API',
@@ -23,15 +24,14 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
     patterns=schema_url_patterns,
 )
+'''
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
-    # url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
+'''
 if settings.DEBUG:
     urlpatterns += [
         re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
@@ -40,3 +40,4 @@ if settings.DEBUG:
     ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+'''
