9675221efb8052ce589a8f0cec49bf878a12db84
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-07-16 22:01:21 +0900

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -7,36 +7,17 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
-schema_url_patterns = [
-    path('api/', include('api.urls'))
-]
-schema_view = get_schema_view(
-    openapi.Info(
-        title='Snippets API',
-        default_version='v1',
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License")
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-    patterns=schema_url_patterns,
-)
+router = routers.DefaultRouter()
+router.register('rooms', views.RoomViewSet)
+router.register('room-price', views.RoomPriceViewSet)
+router.register('sample-data', views.SampleDataViewSet)
+router.register('course', views.CourseViewSet)
+router.register('course-price', views.CoursePriceViewSet)
+router.register('payment', views.PaymentViewSet)
+router.register('reservation', views.ReservationViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    # url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include(router.urls)),
 ]
 
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
-    ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
