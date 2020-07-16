d0d7c399b39470a160883b817e7f76c647c6c296
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-07-16 21:51:12 +0900

    Update urls.py

--- api_server/urls.py
+++ api_server/urls.py
@@ -7,11 +7,11 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
+'''
 schema_url_patterns = [
     path('api/', include('api.urls'))
 ]
-'''
+
 schema_view = get_schema_view(
     openapi.Info(
         title='Snippets API',
