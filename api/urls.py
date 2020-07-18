ec9a2e36b9ba5c8c3da033e103e562910358cbde
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-07-18 16:49:40 +0900

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import routers
+from . import views
 
 router = routers.DefaultRouter()
 router.register('rooms', views.RoomViewSet)
