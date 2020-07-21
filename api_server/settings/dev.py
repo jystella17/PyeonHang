bc54e3c4f3f91264542915ea890df5c5bcd16a9a
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-07-21 23:37:31 +0900

    Update dev.py

--- api_server/settings/dev.py
+++ api_server/settings/dev.py
@@ -1,2 +1,17 @@
 import os
 from .base import *
+
+DEBUG = TRUE
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+  'default': {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ph2020",
+        "USER": "root",
+        "PASSWORD": "theo93",
+        "HOST" : "localhost",
+        "PORT": "3306"
+    }
+}
