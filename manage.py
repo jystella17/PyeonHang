2c02f191288091c2426b3dfd2d7db3e20b877226
jystella17 <42269221+jystella17@users.noreply.github.com>
2020-07-22 20:47:56 +0900

    Update manage.py

--- manage.py
+++ manage.py
@@ -18,6 +18,4 @@ def main():
 
 
 if __name__ == '__main__':
-    os.environ.setdefault("DJANGO_SETTING_MODULE", "api_server.settings")
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
+    main()
